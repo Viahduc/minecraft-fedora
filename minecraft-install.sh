8b4a926f560df8193476b64fd3b4c498d6265689
Viahduc <82482523+Viahduc@users.noreply.github.com>
2022-03-15 22:11:51 -0400

    Add files via upload

--- minecraft-install.sh
+++ minecraft-install.sh
@@ -2,7 +2,11 @@
 wget https://launcher.mojang.com/download/Minecraft.tar.gz
 tar xf Minecraft.tar.gz
 cd minecraft-launcher
-mv minecraft-launcher /usr/bin/
+sudo mv minecraft-launcher /usr/bin/
 sudo mv minecraft.desktop /usr/share/applications/minecraft.desktop
 sudo mv minecraft.png /usr/share/icons/
+cd ..
+rm -rf minecraft-launcher
+cd ..
+rm -rf Minecraft.tar.gz
 
