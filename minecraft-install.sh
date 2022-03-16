ccb40cec9e74b40c214182bd3b7eb4a5de750a8d
Viahduc <82482523+Viahduc@users.noreply.github.com>
2022-03-15 22:24:00 -0400

    Update minecraft-install.sh

--- minecraft-install.sh
+++ minecraft-install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+echo "Downloading Minecraft Launcher..."
 wget https://launcher.mojang.com/download/Minecraft.tar.gz
 tar xf Minecraft.tar.gz
 cd minecraft-launcher
+echo "Installing Minecraft Launcher..."
 sudo mv minecraft-launcher /usr/bin/
 sudo mv minecraft.desktop /usr/share/applications/
 sudo mv minecraft.png /usr/share/icons/
@@ -9,4 +11,5 @@ cd ..
 rm -rf minecraft-launcher
 cd ..
 rm -rf Minecraft.tar.gz
+echo "Minecraft Launcher installed!"
 
